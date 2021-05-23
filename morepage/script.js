4b3458a2b3e989433257a9f28232ce6c0a6d1172
pythagoras26 <barconadia38@gmail.com>
2021-05-23 03:05:41 -0300

    .

---
+++ morepage/script.js
@@ -0,0 +1,20 @@
+
+/*var menuBtn = document.getElementById("sasa")
+var menu = document.getElementById("show")
+
+document.getElementById("sasa").addEventListener("click", function(){
+    if(menuBtn.click()){
+        return menu;
+    }
+})
+function myFunction(menuBtn){
+   if(menuBtn.click()){
+       return menu;
+   }
+};
+*/
+ function myFunction(){
+     var x=document.getElementById("show");
+     alert(x);
+
+ }

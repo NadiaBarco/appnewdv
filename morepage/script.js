132988cffacd3aa7caccdc49374710f8f42c7b24
pythagoras26 <barconadia38@gmail.com>
2021-05-23 21:41:00 -0300

    end basic

--- morepage/script.js
+++ morepage/script.js
@@ -1,20 +1,6 @@
+const btn =document.querySelector('span');
+      sa =document.querySelector('show');
 
-/*var menuBtn = document.getElementById("sasa")
-var menu = document.getElementById("show")
-
-document.getElementById("sasa").addEventListener("click", function(){
-    if(menuBtn.click()){
-        return menu;
-    }
+btn.addEventListener('click', (e)=>{
+    
 })
-function myFunction(menuBtn){
-   if(menuBtn.click()){
-       return menu;
-   }
-};
-*/
- function myFunction(){
-     var x=document.getElementById("show");
-     alert(x);
-
- }
